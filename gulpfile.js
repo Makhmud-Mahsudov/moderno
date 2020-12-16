3da452f56019c6967df749fff2b7c6fdf21dbd55
Makhmud-Mahsudov <mahsudovmahmud19@gmail.com>
2020-12-16 18:14:41 +0500

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ let gulp = require('gulp'),
 const { version } = require('os');
 
 gulp.task('sass', function(){
-  return gulp.src('app/scss/style.scss')
+  return gulp.src('app/scss/**/*.scss')
           .pipe(sass({outputStyle: 'compressed'}))
           .pipe(rename({suffix: '.min'}))
           .pipe(autoprefixer({
@@ -41,7 +41,7 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('watch', function(){
-  gulp.watch('app/scss/style.scss', gulp.parallel('sass'))
+  gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'))
   gulp.watch('app/*.html', gulp.parallel('html'))
   gulp.watch('app/js/*.js', gulp.parallel('js'))
 });
